dea6c4b3cb706421f7fe8cf8d478a3be5b750888
unknown <128090555+Robinmk77@users.noreply.github.com>
2025-04-04 10:39:32 -0500

    WIP header and footer

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,39 @@
 <header>
-
+    <div class="content-box flex gap-4 justify-between">
+        <div>
+            <img src="/images/logo.png" alt="Johnson Insurance Group" width="380px" height="auto">
+        </div>
+        <nav>
+            <div class="flex justify-end gap-3 mb-4">
+                <a class="block px-5 py-2 border-2 border-brand-lime" href="">Contact Us</a>
+                <a class="block px-5 py-2 border-2 border-brand-lime" href="">Pay My Bill</a>
+            </div>
+            <ul class="flex justify-between w-full gap-8 text-xl font-semibold">
+                <li class="hover:underline hover:text-brand-lime">
+                    <a href="">Home</a>
+                </li>
+                <li class="hover:underline hover:text-brand-lime">
+                    <a href="" class="flex items-center gap-2">
+                        Business Insurance
+                        <svg class="mt-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+                            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="4" d="M36 18L24 30L12 18" />
+                        </svg>
+                    </a>
+                </li>
+                <li class="hover:underline hover:text-brand-lime">
+                    <a href="" class="flex items-center gap-2">
+                        Personal Insurance
+                        <svg class="mt-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+                            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="4" d="M36 18L24 30L12 18" />
+                        </svg>
+                    </a>
+                </li>
+                <li class="hover:underline hover:text-brand-lime">
+                    <a href="">Customer Service</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
