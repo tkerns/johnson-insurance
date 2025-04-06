6d927f2b7169644e9e75880c95025046b2a48044
unknown <128090555+Robinmk77@users.noreply.github.com>
2025-04-06 08:47:05 -0500

    small button styling fixes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,15 +5,15 @@
         </div>
         <nav>
             <div class="flex justify-end gap-3 mb-4">
-                <a class="block px-5 py-2 border-2 border-brand-lime hover:bg-black focus:bg-black hover:text-white focus:text-white" href="/contact">Contact Us</a>
-                <a class="block px-5 py-2 border-2 border-brand-lime hover:bg-black focus:bg-black hover:text-white focus:text-white" href="https://johnsoninsuranceak.epaypolicy.com/">Pay My Bill</a>
+                <a class="block px-5 py-2 border-2 border-brand-lime hover:bg-brand-lime focus:bg-brand-lime hover:text-white focus:text-white" href="/contact">Contact Us</a>
+                <a class="block px-5 py-2 border-2 border-brand-lime hover:bg-brand-lime focus:bg-brand-lime hover:text-white focus:text-white" href="https://johnsoninsuranceak.epaypolicy.com/">Pay My Bill</a>
             </div>
             <ul class="flex justify-between w-full gap-8 text-xl font-semibold">
                 <li class="hover:underline hover:text-brand-lime">
                     <a href="/">Home</a>
                 </li>
                 <li class="hover:underline hover:text-brand-lime">
-                    <button class="flex items-center gap-2 cursor-pointer">
+                    <button class="flex items-center gap-2">
                         Business Insurance
                         <svg class="mt-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
                             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -22,7 +22,7 @@
                     </button>
                 </li>
                 <li class="hover:underline hover:text-brand-lime">
-                    <button class="flex items-center gap-2 cursor-pointer">
+                    <button class="flex items-center gap-2">
                         Personal Insurance
                         <svg class="mt-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
                             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
