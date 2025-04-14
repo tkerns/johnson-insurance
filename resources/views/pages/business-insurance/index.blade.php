b0a604498bb767dde2c50283fbd3c38fd5d85b11
unknown <128090555+Robinmk77@users.noreply.github.com>
2025-04-14 12:23:37 -0500

    create business+personal insurance pages

--- resources/views/pages/business-insurance/index.blade.php
+++ resources/views/pages/business-insurance/index.blade.php
@@ -1,4 +1,121 @@
 @extends('layouts.main')
 @section('content')
-
+    <div class="h-[550px] bg-no-repeat bg-cover bg-center"
+        style="background-image: url('/images/inside-header.jpg')">
+        <div class="h-full content-box text-white ml-auto flex flex-col justify-center items-center">
+            <hr class="mx-auto border-2 border-brand-lime w-24">
+            <h1 class="text-center my-8">BUSINESS INSURANCE</h1>
+            <hr class="mx-auto border-2 border-brand-lime w-24">
+        </div>
+    </div>
+    <main class="mt-10 content-box grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-1.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Worker's Compensation</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-2.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>General Liability</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-3.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Commercial Property</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-4.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Commercial Auto and Trucking</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-5.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Equipment Insurance</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-6.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Ocean Marine</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-7.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Aviation</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-8.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Commercial Umbrella</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-9.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Professional Liability</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-10.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Worker's Compensation</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-11.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Worker's Compensation</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col">
+            <img class="w-full" src="/images/business-12.jpg" alt="">
+            <div class="p-8 border-b-2 border-l-2 border-r-2 border-black h-full">
+                <h2>Worker's Compensation</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+    </main>
 @endsection
--- resources/views/pages/personal-insurance/index.blade.php
+++ resources/views/pages/personal-insurance/index.blade.php
@@ -1,4 +1,76 @@
 @extends('layouts.main')
 @section('content')
-
+    <div class="h-[550px] bg-no-repeat bg-cover bg-center"
+        style="background-image: url('/images/inside-header.jpg')">
+        <div class="content-box text-white ml-auto h-full flex flex-col justify-center items-center">
+            <hr class="mx-auto border-2 border-brand-lime w-24">
+            <h1 class="text-center my-8">PERSONAL INSURANCE</h1>
+            <hr class="mx-auto border-2 border-brand-lime w-24">
+        </div>
+    </div>
+    <main class="mt-10 content-box grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <section class="flex flex-col border-2 border-black">
+            <img class="w-full h-full" src="/images/business-1.jpg" alt="">
+            <div class="p-8">
+                <h2>Homeowner's Insurance</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col border-2 border-black">
+            <img class="w-full h-full" src="/images/business-1.jpg" alt="">
+            <div class="p-8">
+                <h2>Personal Auto</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col border-2 border-black">
+            <img class="w-full h-full" src="/images/business-1.jpg" alt="">
+            <div class="p-8">
+                <h2>Watercraft</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col border-2 border-black">
+            <img class="w-full h-full" src="/images/business-1.jpg" alt="">
+            <div class="p-8">
+                <h2>Snowmachine</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col border-2 border-black">
+            <img class="w-full h-full" src="/images/business-1.jpg" alt="">
+            <div class="p-8">
+                <h2>RV</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col border-2 border-black">
+            <img class="w-full h-full" src="/images/business-1.jpg" alt="">
+            <div class="p-8">
+                <h2>ATV</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+        <section class="flex flex-col border-2 border-black">
+            <img class="w-full h-full" src="/images/business-1.jpg" alt="">
+            <div class="p-8">
+                <h2>Personal Umbrella</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero rem nemo pariatur labore nisi
+                    et, perspiciatis laudantium aliquid minima quisquam! Fugiat nisi veritatis culpa debitis earum assumenda
+                    quasi sunt!</p>
+            </div>
+        </section>
+    </main>
 @endsection
